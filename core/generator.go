01fb496c9e8d915afd846309d9760fca68819bc1
Yash Joshi <yashjoshi538@bitgo.com>
2025-06-11 13:41:43 -0400

    refactor(core): remove redundant func in generator, add comments
    BTC-2144

--- core/generator.go
+++ core/generator.go
@@ -6,14 +6,20 @@ import (
 	"bitgo.com/proof_of_reserves/circuit"
 )
 
-func writeTestDataToFile(batchCount int, countPerBatch int) {
+// GenerateData generates test data and writes it to files for development/testing purposes.
+func GenerateData(batchCount int, countPerBatch int) {
 	var lastAccount *circuit.GoAccount
+
 	for i := 0; i < batchCount; i++ {
 		filePath := "out/secret/test_data_" + strconv.Itoa(i) + ".json"
+
+		// generate data for single batch
 		var secretData ProofElements
 		var assetSum circuit.GoBalance
 		secretData.Accounts, assetSum, secretData.MerkleRoot, secretData.MerkleRootWithAssetSumHash = circuit.GenerateTestData(countPerBatch, i+11)
 		secretData.AssetSum = &assetSum
+
+		// write to file
 		err := writeJson(filePath, ConvertProofElementsToRawProofElements(secretData))
 		if err != nil {
 			panic(err)
@@ -25,13 +31,10 @@ func writeTestDataToFile(batchCount int, countPerBatch int) {
 	if lastAccount == nil {
 		panic("lastAccount is nil")
 	}
+
+	// write last account to separate file to test with
 	err := writeJson("out/user/test_account.json", circuit.ConvertGoAccountToRawGoAccount(*lastAccount))
 	if err != nil {
 		panic(err)
 	}
-}
-
-// GenerateData generates test data and writes it to files for development/testing purposes.
-func GenerateData(batchCount int, countPerBatch int) {
-	writeTestDataToFile(batchCount, countPerBatch)
 }
