db3991219493cba9a7e46b49346fb4e65c5f03bb
Yash Joshi <yashjoshi538@bitgo.com>
2025-06-18 13:00:15 -0400

    feat(core): updated VerifyProofPath and Verify (and changed names).
    Changed Verify to VerifyFull - made logic so that verifies merkle paths and verifies inclusion of every account. Changed VerifyProofPath to VerifyUser.
    
    BTC-2208

--- core/verifier.go
+++ core/verifier.go
@@ -58,6 +58,9 @@ func verifyProof(proof CompletedProof) error {
 
 // verifyMerklePath verifies that a particular hash and merkle path lead to the given merkle root
 func verifyMerklePath(hash Hash, path []Hash, root Hash) error {
+	if len(path) != circuit.TreeDepth {
+		return fmt.Errorf("merkle path is not of depth of tree: expected length %d, found %d", circuit.TreeDepth, len(path))
+	}
 	hasher := mimc.NewMiMC()
 	curr := hash
 	var err error
@@ -75,8 +78,11 @@ func verifyMerklePath(hash Hash, path []Hash, root Hash) error {
 }
 
 // verifyBuild verifies that the given merkle nodes are indeed part of the merkle tree with the given root.
-// assumes depth of tree is len(nodes) - 1.
 func verifyBuild(nodes [][]Hash, root Hash) error {
+	if len(nodes)-1 != circuit.TreeDepth {
+		return fmt.Errorf("expected %d layers of nodes, found %d", circuit.TreeDepth+1, len(nodes))
+	}
+
 	treeDepth := len(nodes) - 1
 	hasher := mimc.NewMiMC()
 
@@ -207,50 +213,99 @@ func verifyInclusionInProof(accountHash circuit.Hash, bottomLayerProofs []Comple
 // that the bottom layer proof's MerkleRootWithAssetSumHash is included in the mid layer proof's MerkleRoot,
 // and repeat the earlier steps for the mid and top layer proofs.
 // It also verifies that the top layer proof's MerkleRootWithAssetSumHash matches the MerkleRoot and published AssetSum.
-func VerifyProofPath(accountHash circuit.Hash, bottomLayerProof CompletedProof, midLayerProof CompletedProof, topLayerProof CompletedProof) {
-	if err := verifyProof(bottomLayerProof); err != nil {
-		panic("Bottom layer proof verification failed: " + err.Error())
-	}
-	if err := verifyProof(midLayerProof); err != nil {
-		panic("Mid layer proof verification failed: " + err.Error())
-	}
-	if err := verifyProof(topLayerProof); err != nil {
-		panic("Top layer proof verification failed: " + err.Error())
-	}
+func VerifyUser(accountHash Hash, accountMerklePath []Hash, bottomLayerProof CompletedProof, midLayerProof CompletedProof, topLayerProof CompletedProof) {
+	// verify proofs
+	panicOnError(verifyProof(bottomLayerProof), "bottom layer proof verification failed")
+	panicOnError(verifyProof(midLayerProof), "mid layer proof verification failed")
+	panicOnError(verifyProof(topLayerProof), "top layer proof verification failed")
 
-	if err := verifyInclusionInProof(accountHash, []CompletedProof{bottomLayerProof}); err != nil {
-		panic("Account not found in bottom layer proof: " + err.Error())
-	}
+	// verify inclusion of account -> bottom proof -> middle proof -> top
+	panicOnError(
+		verifyMerklePath(accountHash, accountMerklePath, bottomLayerProof.MerkleRoot),
+		"failed to verify if account included in bottom proof",
+	)
+	panicOnError(
+		verifyMerklePath(bottomLayerProof.MerkleRootWithAssetSumHash, bottomLayerProof.MerklePath, midLayerProof.MerkleRoot),
+		"failed to verify if account included in bottom proof",
+	)
+	panicOnError(
+		verifyMerklePath(midLayerProof.MerkleRootWithAssetSumHash, midLayerProof.MerklePath, topLayerProof.MerkleRoot),
+		"failed to verify if account included in bottom proof",
+	)
 
-	if err := verifyInclusionInProof(bottomLayerProof.MerkleRootWithAssetSumHash, []CompletedProof{midLayerProof}); err != nil {
-		panic("Bottom layer proof not found in mid layer proof: " + err.Error())
-	}
-
-	if err := verifyInclusionInProof(midLayerProof.MerkleRootWithAssetSumHash, []CompletedProof{topLayerProof}); err != nil {
-		panic("Mid layer proof not found in top layer proof: " + err.Error())
-	}
-
-	if err := verifyTopLayerProofMatchesAssetSum(topLayerProof); err != nil {
-		panic("Top layer proof hash with asset sum does not match published asset sum: " + err.Error())
-	}
+	// verify top layer asset sum (encoded in MerkleRootWithAssetSumHash) matches the published asset sum
+	panicOnError(verifyTopLayerProofMatchesAssetSum(topLayerProof), "top layer hashed asset sum does not match published asset sum")
 }
 
-// Verify should primarily be used to verify the proofs after running prover.
-// Verify verifies that account is included in one of the bottom level proofs, and that every proof is valid and leads
-// to a higher level proof. Verify uses hardcoded file names to read the proofs from disk.
-func Verify(batchCount int, account circuit.GoAccount) {
+// verifyFull is used to perform full verification of generated proofs.
+// It verifies that every account is included in one of the bottom level proofs, and that every proof is valid,
+// has a valid Merkle path leading to the upper level proof, and has the correct merkle nodes for its merkle root.
+// It also verifies the published asset sum in the top level proof matches the sum hashed with the merkle root.
+// Expects that all the CompletedProofs read will contain MerkleNodes to be verified, and expects accounts to be in batches
+// and in the same order they were fed into the proof generator, both at batch level and individual level.
+func verifyFull(bottomLevelProofs, midLevelProofs []CompletedProof, topLevelProof CompletedProof, accountBatches [][]circuit.GoAccount) {
 
+	// bottom level proofs (verify merkle nodes, proof, merkle path)
+	for i, bottomProof := range bottomLevelProofs {
+		panicOnError(
+			verifyBuild(bottomProof.MerkleNodes, bottomProof.MerkleRoot),
+			fmt.Sprintf("merkle nodes for bottom level proof %d inconsistent with its merkle root", i),
+		)
+		panicOnError(verifyProof(bottomProof), fmt.Sprintf("circuit verification failed for bottom level proof %d", i))
+		panicOnError(
+			verifyMerklePath(bottomProof.MerkleRootWithAssetSumHash, bottomProof.MerklePath, midLevelProofs[i/1024].MerkleRoot),
+			fmt.Sprintf("merkle path verification failed for bottom level proof %d", i),
+		)
+	}
+
+	// mid level proofs (verify merkle nodes, proof, merkle path)
+	for i, middleProof := range midLevelProofs {
+		panicOnError(
+			verifyBuild(middleProof.MerkleNodes, middleProof.MerkleRoot),
+			fmt.Sprintf("merkle nodes for mid level proof %d inconsistent with its merkle root", i),
+		)
+		panicOnError(verifyProof(middleProof), fmt.Sprintf("circuit verification failed for mid level proof %d", i))
+		panicOnError(
+			verifyMerklePath(middleProof.MerkleRootWithAssetSumHash, middleProof.MerklePath, topLevelProof.MerkleRoot),
+			fmt.Sprintf("merkle path verification failed for mid level proof %d", i),
+		)
+	}
+
+	// top level proof (verify merkle nodes and proof)
+	panicOnError(verifyBuild(topLevelProof.MerkleNodes, topLevelProof.MerkleRoot), "merkle nodes for top level proof inconsistent with merkle root")
+	panicOnError(verifyProof(topLevelProof), "top level proof circuit verification failed")
+
+	// verify account inclusion
+	for i, batch := range accountBatches {
+		for j, account := range batch {
+			accountHash := circuit.GoComputeMiMCHashForAccount(account)
+			if !bytes.Equal(accountHash, bottomLevelProofs[i].MerkleNodes[circuit.TreeDepth][j]) {
+				panic(fmt.Sprintf("account %d of batch %d not found in bottom level proofs (or accounts not given in the order given to prover)", j, i))
+			}
+		}
+	}
+
+	// verify top layer asset sum (encoded in MerkleRootWithAssetSumHash) matches the published asset sum
+	panicOnError(verifyTopLayerProofMatchesAssetSum(topLevelProof), "top layer hashed asset sum does not match published asset sum")
+}
+
+// VerifyFull should primarily be used to perform a full verification of the proofs after running prover.
+// Is a wrapper around the private verifyFull and uses hardcoded file names to read the proofs and accounts from disk.
+func VerifyFull(batchCount int) {
+
+	// read accounts
+	proofElements := ReadDataFromFiles[ProofElements](batchCount, "out/secret/test_data_")
+	accounts := make([][]circuit.GoAccount, batchCount)
+	for i, proofElement := range proofElements {
+		accounts[i] = proofElement.Accounts
+	}
+
+	// read proofs from files
 	bottomLevelProofs := ReadDataFromFiles[CompletedProof](batchCount, "out/public/test_proof_")
 	// the number of mid level proofs is ceil(batchCount / 1024)
 	midLevelProofs := ReadDataFromFiles[CompletedProof]((batchCount+1023)/1024, "out/public/test_mid_level_proof_")
 	topLevelProof := ReadDataFromFiles[CompletedProof](1, "out/public/test_top_level_proof_")[0]
 
-	// Verify the proofs
-	verifyProofs(bottomLevelProofs, midLevelProofs, topLevelProof)
-
-	// Verify account inclusion
-	accountHash := circuit.GoComputeMiMCHashForAccount(account)
-	if err := verifyInclusionInProof(accountHash, bottomLevelProofs); err != nil {
-		panic("Account not found in any bottom layer proof: " + err.Error())
-	}
+	// verify
+	verifyFull(bottomLevelProofs, midLevelProofs, topLevelProof, accounts)
 }
