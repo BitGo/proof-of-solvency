67e6fe9a31ecd1069b4fe0fa0a7f85d9c097e379
Yash Joshi <yashjoshi538@bitgo.com>
2025-07-01 16:19:22 -0400

    feat(core): use outDir during seed generation for GenerateData
    This is to enhance randomness across generated data with different outDirs.
    
    BTC-2144

--- core/generator.go
+++ core/generator.go
@@ -8,13 +8,19 @@ import (
 
 // GenerateData generates test data and writes it to files for development/testing purposes.
 func GenerateData(batchCount int, countPerBatch int, outDir string) {
+	// create base seed for generating accounts with outDir
+	baseSeed := 0
+	for i := range outDir {
+		baseSeed ^= int(outDir[i])
+	}
+
+	// for each batch, generate a file with test data
 	for i := 0; i < batchCount; i++ {
 		filePath := outDir + SECRET_DATA_PREFIX + strconv.Itoa(i) + ".json"
 
-		// generate data for single batch
 		var secretData ProofElements
 		var assetSum circuit.GoBalance
-		secretData.Accounts, assetSum, secretData.MerkleRoot, secretData.MerkleRootWithAssetSumHash = circuit.GenerateTestData(countPerBatch, i+11)
+		secretData.Accounts, assetSum, secretData.MerkleRoot, secretData.MerkleRootWithAssetSumHash = circuit.GenerateTestData(countPerBatch, baseSeed+i)
 		secretData.AssetSum = &assetSum
 
 		// write to file
